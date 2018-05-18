81875921a20f01067ec548bb7bcaf09cf4e948d1
hujj0908 <huwanqi@shuwen.com>
2018-05-18 15:59:58 +0800

    huwnaqi: first commit

---
+++ config/config.default.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = exports = {};
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1526629574510_3998';
+
+  // add your config here
+  config.middleware = [];
+
+  config.keys = 'huwanqi';
+  // 添加 view 配置
+  config.view = {
+    defaultViewEngine: 'nunjucks',
+    mapping: {
+      '.tpl': 'nunjucks',
+    },
+  };
+
+  return config;
+};
